f8bb7d1862ff913f76cf270c844094ecd7a1f3e2
Juanezcaa <juanezcaa@gmail.com>
2020-08-10 18:20:40 -0500

    fix amount

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -21,7 +21,7 @@ class _OrderItemState extends State<OrderItem> {
       margin: EdgeInsets.all(10),
       child: Column(children: [
         ListTile(
-          title: Text('\$${widget.order.amount}'),
+          title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
           subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)
           ),
           trailing: IconButton(
